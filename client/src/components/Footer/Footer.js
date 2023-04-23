c62309d3f5241b1e919b106385a307e02f5dfcf7
shivajha <shiva.jha2000@gmail.com>
2023-04-23 19:12:02 +0530

    refferdcard

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,16 +1,9 @@
 import React from "react";
-import {
-  Box,
-  Container,
-  Row,
-  Column,
-  Heading,
-  Text,
-} from "./FooterStyles";
+import { Box, Container, Row, Column, Heading, Text } from "./FooterStyles";
 
 const Footer = () => (
   <Box>
-    <Text>trusted by 150+ companies aroind the world</Text>
+    <Text>trusted by 150+ companies around the world</Text>
     <Container>
       <Row>
         <Column>
