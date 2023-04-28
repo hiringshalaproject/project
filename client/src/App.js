a29605b7117df977b136d3645f6489752baea499
shivajha <shiva.jha2000@gmail.com>
2023-04-29 02:31:02 +0530

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import "./app.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/about";
 import Platform from "./pages/platform";
@@ -18,28 +17,32 @@ import AddSeeker from "./pages/AddSeeker";
 import AddEmployee from "./pages/AddEmployee";
 import GetAllEmployees from "./pages/GetAllEmployee";
 
-
 function App() {
+  const location = useLocation();
+
+  // Check if the current path is "/dashboard"
+  const isDashboardPage = location.pathname === "/dashboard";
+
   return (
-    <div className='w-screen h-screen bg-white flex flex-col'>
-        <Navbar/>
+    <div className="w-screen h-screen bg-white flex flex-col">
+      {!isDashboardPage && <Navbar />}
       <Routes>
-      <Route path="/" element={<Home/>} />
-        <Route path="/admin" element={ <AdminDashboard/> } />
-        <Route path="/add" element={ <AddJob /> } />
-        <Route path="/addSeeker" element={ <AddSeeker /> } />
-        <Route path="/addEmployee" element={ <AddEmployee /> } />
-        <Route path="/getEmployee" element={ <GetAllEmployees /> } />
-        <Route path="/getJobs" element={ <GetAllJobs/>} />
-        <Route path="/getSeekers" element={ <GetAllSeekers/>} />
-        <Route path="/edit" element={ <EditJob/> } />
-        <Route path="/about" element={<About/>} />
-        <Route path="/platform" element={<Platform/>} />
-        <Route path="/contact-us" element={<Contact/>} />
-        <Route path="/Dashboard" element={<Dashboard/>} />
-        <Route path="/login" element = {<Login/>} />
-        <Route path="/signup" element = {<Signup/>} />
-        <Route path="Dashboard" element={<Dashboard/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/add" element={<AddJob />} />
+        <Route path="/addSeeker" element={<AddSeeker />} />
+        <Route path="/addEmployee" element={<AddEmployee />} />
+        <Route path="/getEmployee" element={<GetAllEmployees />} />
+        <Route path="/getJobs" element={<GetAllJobs />} />
+        <Route path="/getSeekers" element={<GetAllSeekers />} />
+        <Route path="/edit" element={<EditJob />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/platform" element={<Platform />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* <Route path="Dashboard" element={<Dashboard/>} /> */}
       </Routes>
     </div>
   );
--- client/src/components/RefferedJobCard/FetchData.js
+++ client/src/components/RefferedJobCard/FetchData.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchUsers = async () => {
-  let response = await axios.get("http://localhost:8000/api/v1/jobs");
+  let response = await axios.post("http://localhost:8000/api/v1/jobs");
   console.log("ResponseData", response.data.jobs);
 
   return response.data.jobs;
