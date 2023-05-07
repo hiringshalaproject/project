fda0aba4a0b54d1cb0a597c4a7367e30760d4c29
unknown <rgupta761999@gmail.com>
2023-05-08 01:12:49 +0530

    homepage is made responsive

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,20 +9,20 @@ const Home = () => (
   <>
     <Container className="mt-5">
       <Row className="justify-content-center align-items-center">
-        <Col lg={6}>
+        <Col xl={6} lg={6} md={6} sm={6}>
           <article>
             <img src={homepage} alt="img" className="img-fluid" />
           </article>
         </Col>
-        <Col lg={6}>
-          <h1 className="mb-4">
+        <Col xl={6} lg={6} md={6} sm={6} className="px-4">
+          <h1 style={{ fontSize: "4vw" }} className="font-bold">
             unleash your talent potential with company name
           </h1>
-          <p className=" md:ml-64 sm:ml-0 text-sm md:text-base mt-4 md:mt-8">
+          <p style={{ fontSize: "1.8vw" }} className="lg:ml-26 md:ml-32 sm:ml-20 xs:ml-0 font-normal ">
             the largest pool of career opportunities that match your skill set.
             link up with outstanding people to create a better future.
           </p>
-
+          
           <Button/>
         </Col>
       </Row>
@@ -30,12 +30,6 @@ const Home = () => (
     <Footer />
   </>
 );
-
-
-
-
-
-
 
 
 export default Home;
