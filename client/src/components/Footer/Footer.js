2c9ca0845c7e4182e7aa22c11d330af6f0b47f80
unknown <rgupta761999@gmail.com>
2023-05-13 11:48:13 +0530

    footer changed & made responsive

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,43 +1,22 @@
 import React from "react";
-import { MDBFooter, MDBContainer, MDBBtn } from "mdb-react-ui-kit";
+import ItemsContainer from "./ItemsContainer";
+import SocialIcons from "../Footer/SocialIcons";
+import { Icons } from "../Footer/Menus";
 
-const Footer = () => (
-  <MDBFooter
-    className="text-center text-white footers"
-    style={{ marginTop: "auto" }}
-  >
-    <MDBContainer className="text-center" style={{ width: 350 }}>
-      <div
-        className="text-dark p-3 rounded-pill m-4"
-        style={{ backgroundColor: "#A8DBD9" }}
-      >
-        Trusted by 150+ companies around the world
-      </div>
-    </MDBContainer>
-    <section className="mb-4">
-      <MDBCompanyCustom name={"Amazon"} />
-      <MDBCompanyCustom name={"Microsoft"} />
-      <MDBCompanyCustom name={"Linkedin"} />
-      <MDBCompanyCustom name={"Juspay"} />
-      <MDBCompanyCustom name={"Google"} />
-    </section>
-  </MDBFooter>
-);
-
-function MDBCompanyCustom({ name }) {
-  const companyTextStyle = {
-    color: "blue", // Change the text color here
-  };
+const Footer = () => {
   return (
-    <MDBBtn
-      color="link"
-      floating
-      size="lg"
-      className="text-secondary mr-12 ml-12 text-decoration-none fw-bold border-0 footer-texts"
-      href=""
-    >
-      <div className="footer-texts">{name}</div>
-    </MDBBtn>
+    <footer className="footers">
+      <ItemsContainer />
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0
+      text-center pt-2 text-sm pb-8 footerTexts"
+      >
+        <span>© 2023 Appy. All rights reserved.</span>
+        <span>Terms · Privacy Policy</span>
+        <SocialIcons Icons={Icons} />
+      </div>
+    </footer>
   );
-}
+};
+
 export default Footer;
