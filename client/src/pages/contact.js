ca0998136e4ffd477918cf8d1421db5e9291ff75
unknown <rgupta761999@gmail.com>
2023-08-15 17:39:03 +0530

    AboutUs and Contact page

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -1,96 +1,84 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
+import contactUs from "../components/assets/contactUs.jpg";
 
-const Contact = () => (
-  <div
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      minHeight: "100vh",
-    }}
-  >
+const Contact = () => {
+
+return(
+<>
+ <div className="mt-20 mx-4 lg:px-10 lg:grid lg:grid-cols-2 gap-2 mb-20">
+    <div className="relative isolate overflow-hidden bg-gray-100 py-24 sm:pt-16 lg:-mb-5 xl:-mb-5 mb-2 rounded-3xl">
+    <img
+        src={contactUs}
+        alt=""
+        className="lg:mt-16 absolute inset-0 -z-10 h-full w-full  object-cover blur-md "
+      />
     <div
-      style={{
-        flex: 1,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "100px",
-      }}
-      className="contact"
-    >
-      <h1>Contact us</h1>
-    </div>
-    <div
-      style={{
-        flex: 1,
-        display: "flex",
-        flexDirection: "column",
-        marginBottom: "50px",
-        marginTop: "50px"
-      }}
-      className="contact"
-    >
-      {/* Contacts */}
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-        }}
+        className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+        aria-hidden="true"
       >
-        <div>
-          <strong>University/College associations</strong>
-          <p>
-            Email us:
-            <a href="mailto:info@hiringshala.com">info@hiringshala.com</a>
-          </p>
-        </div>
-        <div>
-          <strong>Startups hiring</strong>
-          <p>
-            Email us:
-            <a href="mailto:info@hiringshala.com">info@hiringshala.com</a>
-          </p>
-        </div>
-        <div>
-          <strong>Referrals</strong>
-          <p>
-            Email us:
-            <a href="mailto:info@hiringshala.com">info@hiringshala.com</a>
-          </p>
-        </div>
-        <div>
-          <strong>For anything else</strong>
-          <p>
-            Email us:
-            <a href="mailto:info@hiringshala.com">info@hiringshala.com</a>
-          </p>
-        </div>
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
       </div>
-
-
       <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-          marginTop: "30px"
-        }}
+        className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+        aria-hidden="true"
       >
-        <div>
-          <h2>Address</h2>
-          <p>123 Main Street, City, Country</p>
-        </div>
+        <div
+          className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
       </div>
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-bold tracking-tight text-gray-800 sm:text-4xl drop-shadow-lg shadow-gray-900/60">HiringShala  Support</h2>
+            <p className="mt-6 sm:px-3 text-lg leading-8 text-black text-justify drop-shadow-lg shadow-gray-700/60">
+              Thanks for reaching out to us at HiringShala. Our committed and ardent team will be ready to help you.
+            </p>
+            <div className="flex flex-wrap gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+              stroke="currentColor" className=" mt-16 h-6 w-6 text-primary">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+            </svg>
+            <h5 className="mt-16 text-gray-900 font-medium">Our Address:</h5>
+            </div>
+            <p className="text-gray-900 pb-3 sm:pl-12 pl-8 -mb-20">Registered Office: 18th Main Road, 6th Block, Kormangala, Bengaluru, Karnataka 560095 </p>
+          </div>
+        </div>
     </div>
 
+    <div>
+        <div className="bg-gray-100 sm:rounded-r-3xl rounded-3xl">
+           <h4 className="pt-3 pb-2 text-center font-semibold">Facing Technical Problems ?</h4>
+           <p className="px-8 pb-3 text-justify">For any technical issues related to display,SignUp,Login,etc. reach out to us at <a href="mailto:info@hiringshala.com">info@hiringshala.com</a>.We will try to address the issue ASAP.</p>
+        </div>
+
+        <div className=" bg-gray-100 sm:rounded-r-3xl rounded-3xl">
+           <h4 className="pt-3 pb-2 text-center font-semibold">Wish to Partner with us ?</h4>
+           <p className="px-8 pb-3 text-justify">To be a partner for referring candidatess send us your proposal at <a href="mailto:info@hiringshala.com">info@hiringshala.com</a>.Our team will connect with you ASAP.</p>
+        </div>
+
+        <div className=" bg-gray-100 sm:rounded-r-3xl rounded-3xl">
+           <h4 className="pt-3 pb-2 text-center font-semibold">Suggestions/Feedback ?</h4>
+           <p className="px-8 pb-3 text-justify">
+We value your input! Please share your thoughts, suggestions, or feedback with us at <a href="mailto:info@hiringshala.com">info@hiringshala.com</a>.Your insights help us improve and serve you better.</p>
+        </div>
+    </div>
+
+ </div>
     <Footer />
-  </div>
-);
+</>
+)
+};
 
 export default Contact;
